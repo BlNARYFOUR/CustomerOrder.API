80fb049f4a49b67277ffe84e969d7e7e9ec37622
BlNARYFOUR <lambertbrend@gmail.com>
2024-09-12 13:47:14 +0200

    Validate email search string

---
+++ CustomerOrder.API/Application/Validation/Validators/CustomerGetListQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using CustomerOrder.API.Domain.Requests.Queries;
+using FluentValidation;
+
+namespace CustomerOrder.API.Application.Validation.Validators;
+
+public class CustomerGetListQueryValidator : AbstractValidator<CustomerGetListQuery>
+{
+    public CustomerGetListQueryValidator()
+    {
+        RuleFor(q => q.EmailSearch).Must(ValidateSearchString).WithMessage("'Email' cannot be an empty string.");
+    }
+
+    private bool ValidateSearchString(string? searchString)
+    {
+        return "" != searchString;
+    }
+}
